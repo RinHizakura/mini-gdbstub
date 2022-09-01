895ac336deeb06938717e3fde64914372dd3ca89
RinHizakura <s921975628@gmail.com>
2022-09-02 00:45:57 +0800

    Avoid to use extension on debug utils

--- include/utils/log.h
+++ include/utils/log.h
@@ -7,17 +7,8 @@
 void __die(const char *format, ...);
 void __info(const char *format, ...);
 
-/* FIXME: possibly check if the ANSI color codes are supported? */
-#define RED "\x1b[1;31m"
-#define YELLOW "\x1b[1;33m"
-#define BLUE "\x1b[1;94m"
-#define NC "\x1b[0m"
+#define die(format, ...) __die("ERRNO: %s\n" format, strerror(errno))
+#define warn(format, ...) __info("ERRNO: %s\n" format, strerror(errno))
+#define info(format, ...) __info(format)
 
-#define die(format, ...)               \
-    __die(RED "ERRNO: %s\n" NC format, \
-          strerror(errno) __VA_OPT__(, ) __VA_ARGS__)
-#define warn(format, ...)                  \
-    __info(YELLOW "ERRNO: %s\n" NC format, \
-           strerror(errno) __VA_OPT__(, ) __VA_ARGS__)
-#define info(format, ...) __info(BLUE format NC __VA_OPT__(, ) __VA_ARGS__)
 #endif
